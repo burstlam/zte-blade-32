9fddafbe5b9f5be38d1f73eb8d05cf6e33f2d2da
Andy572 <17andy@gmx.de>
2011-05-21 21:39:58 +0200

    decrease the wait time to 10ms

--- drivers/gpu/msm/kgsl_yamato.c
+++ drivers/gpu/msm/kgsl_yamato.c
@@ -647,7 +647,7 @@ kgsl_yamato_init(struct kgsl_device *device, struct kgsl_devconfig *config)
 	/* The core is in an indeterminate state until the reset completes
 	 * after 50ms.
 	 */
-	msleep(50);
+	msleep(10);
 
 	kgsl_yamato_regwrite(device, REG_RBBM_SOFT_RESET, 0x00000000);
 
